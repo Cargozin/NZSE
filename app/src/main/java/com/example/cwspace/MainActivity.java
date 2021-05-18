d2eeed35a5b2d6b269a04d1d928832d0b72f6ca6
Cargozin <74237674+Cargozin@users.noreply.github.com>
2021-05-18 14:44:13 +0200

    frontpage updated

--- app/src/main/java/com/example/cwspace/MainActivity.java
+++ app/src/main/java/com/example/cwspace/MainActivity.java
@@ -1,17 +1,40 @@
 package com.example.cwspace;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.ui.AppBarConfiguration;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button coworkerB;
+    private Button maklerB;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //to be continued in the next chapter
+        coworkerB = findViewById(R.id.buttonCoworker);
+        coworkerB.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                //Code here executes after pressing button
+                //setContentView(R.layout.activity_main);
+            }
+        });
 
+        maklerB = findViewById(R.id.buttonMakler);
+        maklerB.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                //Code here executes after pressing button
+                //setContentView(R.layout.makler);
+            }
+        });
+
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.buttonMakler, R.id.buttonCoworker).build();
     }
+
 }
