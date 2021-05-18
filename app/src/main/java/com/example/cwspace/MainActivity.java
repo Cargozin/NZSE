eec6bb4e1c149d9bfaa8740db1e948603c37c1e7
LordGrim2001 <84382165+LordGrim2001@users.noreply.github.com>
2021-05-18 13:02:07 +0200

    Das ist nur ein Kommentar

--- app/src/main/java/com/example/cwspace/MainActivity.java
+++ app/src/main/java/com/example/cwspace/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //to be continued
+        //to be continued in the next chapter
 
     }
 }
