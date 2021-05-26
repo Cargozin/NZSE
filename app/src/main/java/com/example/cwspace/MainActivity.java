7cbc39a0d1093c066e6593e7bdcf10368d5b7895
LordGrim2001 <84382165+LordGrim2001@users.noreply.github.com>
2021-05-26 13:49:29 +0200

    navbar funktioniert

--- app/src/main/java/com/example/cwspace/MainActivity.java
+++ app/src/main/java/com/example/cwspace/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.cwspace;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,32 +10,18 @@ import androidx.navigation.ui.AppBarConfiguration;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button coworkerB;
-    private Button maklerB;
-
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        coworkerB = findViewById(R.id.buttonCoworker);
-        coworkerB.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                //Code here executes after pressing button
-                setContentView(R.layout.activity_cw_nav);
-            }
-        });
-
-        maklerB = findViewById(R.id.buttonMakler);
-        maklerB.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                //Code here executes after pressing button
-                setContentView(R.layout.activity_ma_nav);
-            }
-        });
-
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.buttonMakler, R.id.buttonCoworker).build();
+    }
+    public void cwClicked(View view){
+        Intent intent = new Intent(this, CoWorker.class);
+        startActivity(intent);
+    }
+    public void maClicked(View view){
+        //Intent intent = new Intent(this, makler.class);
+        //startActivity(intent);
     }
 
 }
