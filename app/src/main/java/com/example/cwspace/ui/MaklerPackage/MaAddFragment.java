b286529d8bf71f1c1b010111b9938909ddf1430e
Cargozin <74237674+Cargozin@users.noreply.github.com>
2021-06-07 15:42:52 +0200

    updated ma_add fragment deleted wrong code

--- app/src/main/java/com/example/cwspace/ui/MaklerPackage/MaAddFragment.java
+++ app/src/main/java/com/example/cwspace/ui/MaklerPackage/MaAddFragment.java
@@ -15,24 +15,10 @@ import com.example.cwspace.Datenklassen.RoomsArray;
 import com.example.cwspace.R;
 
 public class MaAddFragment extends Fragment {
-    EditText nameEdit,numSeatsEdit;
     @Nullable
     @org.jetbrains.annotations.Nullable
     @Override
     public View onCreateView(LayoutInflater inflater,@Nullable ViewGroup container,@Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_ma_add,container,false);
-    }
-    public void maSaveClicked(View view){
-        nameEdit = view.findViewById(R.id.new_room_name);
-        String name = nameEdit.getText().toString();
-
-        numSeatsEdit=view.findViewById(R.id.editTextNumberSeats);
-        String integer = numSeatsEdit.getText().toString();
-        int numSeats = Integer.parseInt(integer);
-
-        RoomsArray.getInstance().add(new Room("test",23));
-
-        Toast toast = Toast.makeText(getContext(), "Saved", Toast.LENGTH_SHORT);
-        toast.show();
     }
 }
